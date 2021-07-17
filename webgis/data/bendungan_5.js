59b2979a15f125f41619768385b24b8612e445fd
yusufWAI <yusupridermask@gmail.com>
2021-07-18 04:52:35 +0700

    menambahkan peta

---
+++ webgis/data/bendungan_5.js
@@ -0,0 +1,17 @@
+var json_bendungan_5 = {
+"type": "FeatureCollection",
+"name": "bendungan_5",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": "1", "bendungan": "Bendungan Gembiro, Krandon" }, "geometry": { "type": "Point", "coordinates": [ 109.538837311753198, -7.00232217573172 ] } },
+{ "type": "Feature", "properties": { "id": "2", "bendungan": "Bendungan Brondong" }, "geometry": { "type": "Point", "coordinates": [ 109.527981018574877, -7.048434144643877 ] } },
+{ "type": "Feature", "properties": { "id": "3", "bendungan": "Bendungan Kali Boro" }, "geometry": { "type": "Point", "coordinates": [ 109.598018867499491, -6.977729974648262 ] } },
+{ "type": "Feature", "properties": { "id": "4", "bendungan": "Bendungan Sokoyoso" }, "geometry": { "type": "Point", "coordinates": [ 109.599227343156087, -7.054480250489531 ] } },
+{ "type": "Feature", "properties": { "id": "5", "bendungan": "Bendungan 2 Embong" }, "geometry": { "type": "Point", "coordinates": [ 109.627586626315434, -7.043312762665193 ] } },
+{ "type": "Feature", "properties": { "id": "6", "bendungan": "Bendungan Padurekso" }, "geometry": { "type": "Point", "coordinates": [ 109.627917355595841, -7.046360757206144 ] } },
+{ "type": "Feature", "properties": { "id": "7", "bendungan": "Bendungan Sungai Kletak" }, "geometry": { "type": "Point", "coordinates": [ 109.648411388976399, -6.979483825412694 ] } },
+{ "type": "Feature", "properties": { "id": "8", "bendungan": "Bendungan Dewang Asri" }, "geometry": { "type": "Point", "coordinates": [ 109.631483554806707, -6.934463542621206 ] } },
+{ "type": "Feature", "properties": { "id": "9", "bendungan": "Bendungan Asem Siketek" }, "geometry": { "type": "Point", "coordinates": [ 109.697488345601968, -6.964709779858236 ] } },
+{ "type": "Feature", "properties": { "id": "10", "bendungan": "Bendungan Simbang" }, "geometry": { "type": "Point", "coordinates": [ 109.676101472187469, -7.004188156414913 ] } }
+]
+}
